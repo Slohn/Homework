5b1fa1856dd7242e3915588adfdef611fc39feeb
Slohn <makarikarustamian@gmail.com>
2020-10-29 15:24:05 +0400

    cosmetic changes

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,42 +7,9 @@ public class Main {
         int r = random.nextInt(10000);
         PassengerCar toyota = new PassengerCar(r);
         toyota.setMax(3500);
-        // int r2 = random.nextInt(3500);
-        int r2 = 1000;
+        int r2 = random.nextInt(3500);
         CargoCar kamaz = new CargoCar(r2);
         kamaz.setMax(10000);
 
     }
 }
-
-  /*  public abstract class Car {
-        private int max;
-
-        public int getMax() {
-            return max;
-        }
-
-        public void setMax(int max) {
-            this.max = max;
-
-        }
-    }
-
-    public class PassengerCar extends Car {
-        int weight;
-
-        public PassengerCar(int weight) {        //конструктор
-            this.weight = weight;
-
-        }
-    }
-
-    public class CargoCar extends Car {
-        int weight;
-
-        public CargoCar(int weight) {
-            this.weight = weight;
-        }
-    }
-
-   */
