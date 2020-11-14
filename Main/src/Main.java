35ce08780b9292e59c8902cb994e30725a1770df
Slohn <makarikarustamian@gmail.com>
2020-11-14 20:13:15 +0400

    Taxi app task

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,12 +1,50 @@
-
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        Station st = new Station();
-        st.setPassengerCars("Tesla");
-        st.setPassengerCars("KIA");
-        st.setCargoCars("Kamaz");
-        st.setCargoCars("Ural");
-        st.getCargoCars();
+        JFrame a = new JFrame("Taxi");
+        JButton button = new JButton("click me");
+        JTextField textField = new JTextField("");
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        a.setSize(300, 300);
+        textField.setBounds(50, 10, 200, 30);
+        button.setBounds(40, 90, 85, 20);
+        a.add(button);
+        a.add(textField);
+        a.setLayout(null);
+        a.setVisible(true);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String getValue = textField.getText();
+                Pattern pat = Pattern.compile("[-]?[0-9]+(.[0-9]+)?");
+                Matcher matcher = pat.matcher(getValue);
+                int peopleCars;
+                int cargoCars;
+                if(matcher.find()){
+                     peopleCars = Integer.parseInt(matcher.group());
+                    if(peopleCars%4 != 0) {
+                        peopleCars = peopleCars/4 + 1;
+                    } else if(peopleCars%4 == 0){
+                        peopleCars = peopleCars/4;
+                    }
+
+                    if(matcher.find()){
+                       cargoCars = Integer.parseInt( matcher.group());
+                       if(cargoCars%3500 != 0){
+                           cargoCars = cargoCars/3500 + 1;
+                       } else if(cargoCars%3500 == 0) {
+                           cargoCars = cargoCars/3500;
+                       }
+                        System.out.println(peopleCars + cargoCars);
+                        JOptionPane.showMessageDialog(null,"Заказано " + String.valueOf(peopleCars + cargoCars) + " легковых машины");
+                    }
+                }
+            }
+        });
+        }
     }
-}
